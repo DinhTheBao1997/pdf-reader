bffdfa970d8559eb97e465cfceec3e2861071350
BaoDT <baodt@imlink.vn>
2023-10-17 13:50:22 +0700

    add scan function

--- main.py
+++
@@ -1,9 +0,0 @@
-from pdfquery import PDFQuery
-from lxml import etree
-
-pdf = PDFQuery('sample.pdf')
-pdf.load()
-
-# Use CSS-like selectors to locate the elements
-text_elements = pdf.pq('LTTextLineHorizontal').text()
-print(text_elements)
